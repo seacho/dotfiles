30bd62ecea511a6c263f9422d50e1f88c08701c0
seacho <p1ain0@outlook.com>
2025-11-30 23:19:55 +0800

    pyenv settings and console mode

--- emacs.d/init2.el
+++ emacs.d/init2.el
@@ -1,5 +1,5 @@
 ;; ==============================================
-;; Ultra-light Emacs config for RE + LSP + Org
+;; Ultra-light Emacs config for RE + Org
 ;; ==============================================
 
 
@@ -11,27 +11,65 @@
      ("http"     . "127.0.0.1:12334")  ; Replace with your proxy IP:PORT
      ("https"    . "127.0.0.1:12334"))) ; Replace with your proxy IP:PORT
 
-
 ;; ========== 包管理 ==========
+
+;; (load "~/.emacs.d/emacs.rc/rc.el")
+
+;; (load "~/.emacs.d/emacs.rc/misc-rc.el")
+;; (load "~/.emacs.d/emacs.rc/org-mode-rc.el")
+;; (load "~/.emacs.d/emacs.rc/autocommit-rc.el")
+
+
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+
+
+;; 包管理
+(defvar rc/package-contents-refreshed nil)
+
+(defun rc/package-refresh-contents-once ()
+  (when (not rc/package-contents-refreshed)
+    (setq rc/package-contents-refreshed t)
+    (package-refresh-contents)))
+
+(defun rc/require-one-package (package)
+  (when (not (package-installed-p package))
+    (rc/package-refresh-contents-once)
+    (package-install package)))
+
+(defun rc/require (&rest packages)
+  (dolist (package packages)
+    (rc/require-one-package package)))
+
+(rc/require-one-package 'use-package)
+
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+
 
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package dash
+  :ensure t
+  :demand t)
+
+(use-package dash-functional
+  :ensure t
+  :demand t)
 
 
 ;; ========== 基础 UI ==========
 ;; Disable all graphical UI elements to focus on code
-(tool-bar-mode -1)           ; specific to Tsoding: hates icons
+(tool-bar-mode -1)           ; hide the tool bar
 (menu-bar-mode -1)           ; learn the hotkeys instead
-(scroll-bar-mode -1)         ; wastes screen real estate
+(when (display-graphic-p)
+  (scroll-bar-mode -1))
+
 (setq inhibit-startup-screen t) ; jump straight to scratch buffer
 (global-display-line-numbers-mode 1)
 (setq ring-bell-function 'ignore)
@@ -52,31 +90,6 @@
 ;;   :config
 ;;   (treesit-auto-add-to-auto-mode-alist 'all)
 ;;   (global-treesit-auto-mode))
-
-;; ---- lsp-mode ----
-;; (use-package lsp-mode
-;;   :hook(
-;;          (c-mode . lsp)
-;;          (c++-mode . lsp)
-;;          (python-mode . lsp)
-;;          (rust-mode . lsp)
-;;          (go-mode . lsp)
-;;          (sh-mode . lsp)
-;;          ;; (c-ts-mode . lsp)
-;;          ;; (c++-ts-mode . lsp)
-;;          ;; (python-ts-mode . lsp)
-;;          ;; (rust-ts-mode . lsp)
-;;          ;; (go-ts-mode . lsp)
-;;          ;; (bash-ts-mode . lsp)
-;; 	 )
-
-
-;;   :commands lsp
-;;   :config
-;;   (setq lsp-headerline-breadcrumb-enable nil
-;;         lsp-enable-symbol-highlighting t
-;;         lsp-enable-snippet nil))
-
 
 ;;; Whitespace mode
 (defun rc/set-up-whitespace-handling ()
@@ -154,14 +167,22 @@
 ;; ---------------------------------------------------------
 ;; COMPILATION
 ;; ---------------------------------------------------------
-;; Tsoding often compiles C code directly from Emacs.
+;; Compiles C code directly from Emacs.
 ;; This binds 'F5' to recompile automatically.
-(global-set-key (kbd "<f5>") 'recompile)
+;; (global-set-key (kbd "<f5>") 'recompile)
 
 ;; ---------------------------------------------------------
 ;; CUSTOM FUNCTIONS (Optional but very "Tsoding")
 ;; ---------------------------------------------------------
 ;; He often live-codes C and wants quick execution.
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode 1))
+
+
+
 (defun run-current-file ()
   "Execute the current file based on extension."
   (interactive)
@@ -187,7 +208,10 @@
 ;; ===== Hexdump =====
 (use-package hexl
   :ensure nil
-  :commands hexl-mode)
+  :commands hexl-mode
+  :config
+  (setq hexl-bits 8)
+  (setq hexl-iso 1))
 
 ;; 打开二进制文件自动 hexl
 (add-to-list 'magic-mode-alist '("\0\0\0\0" . hexl-mode))
@@ -213,4 +237,18 @@
 
 (global-set-key (kbd "C-c d") 'disasm-buffer)
 
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ccls counsel dash-functional go-mode gruber-darker-theme json-mode
+	  magit python-mode pyvenv rust-mode treesit-auto web-mode
+	  yaml-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
